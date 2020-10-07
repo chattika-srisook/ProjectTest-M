9810946e7cd2d65811323a5ae6e9640573d36269
chattika-srisook <chattika.sr@rmuti.ac.th>
2020-10-07 16:05:48 +0700

    ทำต่อจากเอ็ม เพิ่มหน้า Menu

---
+++ app/src/main/java/com/jirapat/todoapp/Order/OrderFragment.kt
@@ -0,0 +1,38 @@
+package com.jirapat.todoapp.Order
+
+import android.os.Bundle
+import android.view.*
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.jirapat.todoapp.R
+import com.jirapat.todoapp.databinding.FragmentToDoBinding
+
+class OrderFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        val binding = DataBindingUtil.inflate<FragmentToDoBinding>(
+            inflater,
+            R.layout.fragment_add_order,
+            container,
+            false
+        )
+        setHasOptionsMenu(true)
+        return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.option_menu, menu)
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(
+            item!!,
+            view!!.findNavController()
+        ) || super.onOptionsItemSelected(item)
+    }
+}
